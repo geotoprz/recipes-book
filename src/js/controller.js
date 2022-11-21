1559b38f1df5a64147dc28167e1b2883394e8ff1
Topalidis <gtopalidis@deloitte.gr>
2022-11-21 16:04:37 +0200

    initial show recipe api

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector(".recipe");
+const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +11,39 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+// Get All Recipes / Create Recipe
+const getAllUrl = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+
+// Get Recipe / Delete Recipe
+const modifySingleUrl = 'https://forkify-api.herokuapp.com/api/v2/recipes/:id';
+
+const key = 'a8c8b207-0bd6-44b5-93f1-ac814aa7b396';
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      serving: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
