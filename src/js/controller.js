6b2d9ef8f196c1c9571a67c64d6b64ca1dc4a1c4
Topalidis <gtopalidis@deloitte.gr>
2022-11-22 17:51:52 +0200

    pagination implementation

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime';
@@ -40,15 +41,28 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) RENDER RESULTS
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) RENDER INITIAL PAGINATION BUTTONS
+    paginationView.render(model.state.search);
   } catch (err) {
     throw err;
   }
 };
-controlSearchResults();
+
+const controlPagination = function (goToPage) {
+  // 1) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render NEW pagination buttons
+  paginationView.render(model.state.search);
+  console.log(goToPage);
+  console.log('run pagin');
+};
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
